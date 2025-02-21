376199a69f61e48b03c2195b294c6be6e0624288
Bryan Kaperick <bryankaperick@gmail.com>
2025-02-21 18:46:46 +0100

    Remove tournaments (moved to separate repo)

--- src/tournaments/config.jl
+++
@@ -1,2 +0,0 @@
-
-# assign_new_mutations!(tourney, team_to_mutation, winners)
--- src/tournaments/structs.jl
+++
@@ -1,6 +0,0 @@
-struct Tournament
-    games_per_map::Int
-    maps_per_epoch::Int
-    epochs::Int
-    mutation_rule::Symbol
-end
