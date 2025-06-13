b17aa752daf2c055cd3384568f58722783873299
Bryan Kaperick <bryankaperick@gmail.com>
2025-06-13 14:08:06 +0200

    fix benchmarks

--- benchmarks/release_benchmark.sh
+++ benchmarks/release_benchmark.sh
@@ -1,11 +1,11 @@
 cd ../CatanEngine.jl
 git checkout v0.1.0
 cd ../RunScripts
-julia -e "include(\"run.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v0.0.1\")"
+julia -e "include(\"run.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v0.1.0\")"
 cd ../CatanEngine.jl
 git checkout master
 cd ../RunScripts
-julia -e "include(\"run.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v.0.1.0\")"
+julia -e "include(\"run.jl\"); benchmark_one_game(\"configs/benchmark_run.toml\", \"v.0.2.0\")"
 
 cp run.jl ../CatanEngine.jl/benchmarks/
 cp configs/benchmark_run.toml ../CatanEngine.jl/benchmarks/
